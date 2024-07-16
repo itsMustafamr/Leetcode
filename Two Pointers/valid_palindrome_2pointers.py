07303043a9684a043f5bb0e93c1fe6bb754964e1
mrmoh <mohd7@iastate.edu>
2024-07-15 23:08:27 -0500

    Valid palindrome LC problem

---
+++ Two Pointers/valid_palindrome_2pointers.py
@@ -0,0 +1,31 @@
+#this method has O(n) run time and memory only 0(1) so best
+# so to get alphanumeric without the help of built in fn we use
+# ascii values -> use ord to get that
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):  # Corrected from `r < l` to `r > l`
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1 
+        return True
+    
+    def alphaNum(self, c):
+        return  (ord('A') <= ord(c) <= ord('Z') or
+                 ord('a') <= ord(c) <= ord('z') or
+                 ord('0') <= ord(c) <= ord('9'))
+
+# Example usage:
+solution = Solution()
+s = "A man, a plan, a canal: Panama"
+print(solution.isPalindrome(s))  # Output: True
+
+s = "race a car"
+print(solution.isPalindrome(s))  # Output: False
---
+++ Two Pointers/valid_palindrome_only_numChars_no_cases_using_builtin_fn.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newStr = ""
+
+        for c in s:
+            if c.isalnum():
+                newStr += c.lower()
+        return newStr == newStr[::-1] #shortcut to reverse a string
+        
+        #s.split()
